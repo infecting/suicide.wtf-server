bd2ea86585a3a99522a68d6a71729e0b120901b5
saturnz-parotta <al3x9a@protonmail.com>
2020-11-24 12:41:01 -0600

    pet supply plus 4

--- routes/giftcard.js
+++ routes/giftcard.js
@@ -9,7 +9,7 @@ router.get('/giftcard/response/capbefore/:cardno', async (req, res) => {
     res.contentType('application/xml');
     res.send(`<?xml version="1.0" encoding="UTF-8"?>
     <Response>
-    <Wait length="22"/>
+    <Wait length="20"/>
     <GetInput inputType="speech" redirect="true" action="https://api.suicide.wtf/v1/giftcard/response/create/before/${req.params.cardno}"  method="POST"/>
     </Response>`)
 })
